e289231980951b2c75ec050580675d6b2bed46f3
Jaack-S <jack.smith131@icloud.com>
2026-06-22 21:20:01 +0100

    Start of clean.py script focusing on 2019-20, AM and naming structure

--- getdata.py
+++ getdata.py
@@ -28,34 +28,3 @@ def download_vaastav():
 
 if __name__ == "__main__":
     download_vaastav()
-
-
-
-
-
-
-
-
-import numpy as np
-import pandas as pd
-
-
-df = pd.read_csv('data/merged_data.csv')
-
-df = df.sort_values(by=['name', 'GW']).reset_index(drop=True)
-
-# Calculate total points in the next 3 gameweeks (GW1 + GW2 + GW3)
-df['target_points_next_3_gws'] = (
-    df.groupby('name')['total_points'].shift(-1) +
-    df.groupby('name')['total_points'].shift(-2) +
-    df.groupby('name')['total_points'].shift(-3)
-)
-
-# Calculate total points in the next 5 gameweeks
-df['target_points_next_5_gws'] = (
-    df.groupby('name')['total_points'].shift(-1) +
-    df.groupby('name')['total_points'].shift(-2) +
-    df.groupby('name')['total_points'].shift(-3) +
-    df.groupby('name')['total_points'].shift(-4) +
-    df.groupby('name')['total_points'].shift(-5)
-)
---
+++ src/clean.py
@@ -0,0 +1,28 @@
+# Import necessary packages
+import pandas as pd
+
+def clean(df: pd.DataFrame) -> pd.DataFrame:
+    # remove 2019-20 season completely
+    if 'season' in df.columns:
+        df = df[~df['season'].astype(str).str.contains('2019[-_]20')].copy()
+
+    # remove assistant manager data
+    if 'position' in df.columns:
+        df = df[df['position'] != 'AM'].copy()
+
+    # make sure names are in form Firstname_Surname
+    if 'name' in df.columns:
+        df["name"] = df["name"].astype(str).str.strip()
+        df["name"] = df["name"].str.replace(r"\s+", "_", regex=True)
+    
+    # sort data in order
+    df = df.sort_values(by=["season", "name", "GW"]).reset_index(drop=True)
+
+    # group by season and name
+    g = df.groupby(["season", "name"])["total_points"]
+
+    # points for nect 3 and 5 gameweeks
+    df["points_next_three"] = g.shift(-1) + g.shift(-2) + g.shift(-3)
+    df["points_next_five"] = g.shift(-1) + g.shift(-2) + g.shift(-3) + g.shift(-4) + g.shift(-5)
+
+    return df
