e1d9a622cf7467641676a499b7fd7da1b0b49afb
oksmith <oksmith22@gmail.com>
2026-06-29 17:08:42 +0100

    Add some metrics code, notebook checking, and skeleton of visualise.py

---
+++ src/visualise.py
@@ -0,0 +1,20 @@
+"""
+This file contains the code for visualising the metrics calculated in evaluate.py
+"""
+
+def plot_across_seasons():
+    pass
+
+
+def plot_within_season():
+    pass
+
+def plot_model_evaluation():
+    """
+    Chains together a bunch of metrics and plots a "master visualisation"
+
+    1. plot_mae_by_season
+    2. plot_mae_by_gameweek
+    3. plot_predicted_vs_actual
+    """
+    pass
